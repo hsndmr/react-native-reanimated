b1c57ef57ffce6d7bc52ff4a51aed1bf3365105d
Hasan Demir <hasan.demir@tarfin.com>
2022-07-24 11:54:47 +0300

    Create PanGestureHandlerScreen.tsx

---
+++ src/PanGestureHandlerScreen.tsx
@@ -0,0 +1,92 @@
+import { StyleSheet, View } from "react-native";
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+const SIZE = 100.0;
+const CIRCLE_RADIUS = SIZE * 2;
+
+type ContextType = {
+  translateX: number;
+  translateY: number;
+};
+
+export default function PanGestureHandlerScreen() {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const handlePanGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    ContextType
+  >({
+    onStart: (event, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+    onEnd: () => {
+      const distance = Math.sqrt(translateX.value ** 2 + translateY.value ** 2);
+
+      if (distance < CIRCLE_RADIUS) {
+        translateX.value = withSpring(0);
+        translateY.value = withSpring(0);
+      }
+    },
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.circle}>
+        <PanGestureHandler onGestureEvent={handlePanGestureEvent}>
+          <Animated.View style={[styles.square, rStyle]} />
+        </PanGestureHandler>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  square: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: "rgba(128, 0, 128, 0.5)",
+    borderRadius: 20,
+  },
+  circle: {
+    width: CIRCLE_RADIUS * 1.8,
+    height: CIRCLE_RADIUS * 1.8,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: CIRCLE_RADIUS,
+    borderWidth: 5,
+    borderColor: "rgba(128, 0, 128, 0.5)",
+  },
+});
